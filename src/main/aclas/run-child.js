ed24b0ad1cfe2621dddbde16c5152536e0db3399
草鞋没号 <308487730@qq.com>
2021-01-01 10:16:29 +0800

    fix: 通过相对路径避开安装到中文路径问题

--- src/main/aclas/run-child.js
+++ src/main/aclas/run-child.js
@@ -1,3 +1,7 @@
+// process.cwd() 会指向安装目录，这里强行指向文件所在路
+// aclas-cc.node 会以 process.cwd() 寻找 AclasSDK.dll
+process.chdir(/* 当没传递 dll_path 时这里非常重要 */__dirname) // 21-01-01 add
+
 const path = require('path');
 // const aclas = require('../../../build/Debug/aclas.node');
 const aclas = require('../../../build/Release/aclas.node');
@@ -6,7 +10,8 @@ const config = {
   host: '192.168.1.2',
   type: 0x0000,
   filename: path.join(__dirname, 'test/plu.txt'),
-  dll_path: path.join(__dirname, 'Win64/AclasSDK.dll'), // md5: 35b248a0c1c35c39e90d2f17408b6ea4
+  // 如果软件安装到中文路径中，不要传递 dll_path 能避开含中文路径加载 AclasSDK.dll 失败问题
+  dll_path: path.join(__dirname, /* md5: 35b248a0c1c35c39e90d2f17408b6ea4 */'Win64/AclasSDK.dll'),
   extra: 'C++ | stdout',
   debug: true,
 };
